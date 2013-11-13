c2fa743e484a85d8b342b7d766308d76345e0cff
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2013-11-13 20:04:20 +0200

    start 0.1.2-SNAPSHOT version

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject geheimtur "0.1.1"
+(defproject geheimtur "0.1.2-SNAPSHOT"
   :description "a secret door to your Pedestal application"
   :url "http://github.com/propan/geheimtur"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.pedestal/pedestal.service "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+                 [io.pedestal/pedestal.service "0.3.0-SNAPSHOT"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [commons-codec "1.6"]
                  [clj-http "0.7.7"]
                  [ring/ring-codec "1.0.0"]
                  ;; integration tests
                  [javax.servlet/javax.servlet-api "3.0.1" :scope "test"]
-                 [org.slf4j/slf4j-nop "1.7.5" :scope "test"]])
+                 ;; [org.slf4j/slf4j-nop "1.7.5" :scope "test"]
+                 ])
